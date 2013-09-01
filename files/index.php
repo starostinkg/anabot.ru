d1825597ac087d092a85186cd345ef578eb9ca09
DySprozin <kirill@anabot.ru>
2013-09-01 17:31:19 +0400

    files

--- files/index.php
+++ files/index.php
@@ -32,6 +32,19 @@ echo $list_all->startlist;
 
 <div style="z-index: 2; position: relative; left: 4%; width: 96%;" id="flasks" class="flasks"></div>
 <div class="flasks">
+ <div id="add" style="right: 0px; position: absolute; z-index:2; margin-right: 15%; width: 350px;">
+ <b>Добавьте и вы пару файлов:</b><br>
+ <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="350" height="60">
+<param name="movie" value="http://static343.depositfiles.com/flash/DepositUploader_350x60.swf?ref=anabotru&member_passkey=1fvrw5ow4y73al8x&interfaceId=3&lang=RU&lang_xml=http%3A%2F%2Fstatic343.depositfiles.com%2Fflash%2FDepositUploader.xml"></param>
+<param name="menu" value="false"></param>
+<param name="scale" value="noScale"></param>
+<param name="allowFullScreen" value="true"></param>
+<param name="allowscriptaccess" value="always"></param>
+<param name="wmode" value="transparent"></param>
+<embed src="http://static343.depositfiles.com/flash/DepositUploader_350x60.swf?ref=anabotru&member_passkey=1fvrw5ow4y73al8x&interfaceId=3&lang=RU&lang_xml=http%3A%2F%2Fstatic343.depositfiles.com%2Fflash%2FDepositUploader.xml" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" menu="false" scale="noScale" wmode="transparent" width="350" height="60"></embed>
+</object>
+ </div>
+ <div style="position: relative; z-index:1;">
  <table class="red_button">
   <tr style="margin:0px; padding:0px;">
    <td colspan="3" class="red_button_01">&nbsp;</td>
@@ -52,6 +65,7 @@ echo $list_all->startlist;
    <td class="red_button_05">&nbsp;</td>
   </tr>
  </table>
+</div>
 </div>
 
 <div style="position: absolute; top:-5000px; font-size:14pt; width:100%;z-index:5; height:0px;">
