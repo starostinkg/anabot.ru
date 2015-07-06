f14eb4b85e93aa61992fa8e3fe0701a923716c83
DySprozin <kirill@anabot.ru>
2015-07-07 00:03:51 +0300

    fix

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ include ROOT . 'inc/header_common.htm';
   <br class="break">
   <img src="/img/libgen.gif" alt="Бесплатные научные статьи" class="img_right_menu">
   <div class="border_right_menu" style="padding-top: 0px;">
-   <a target="_blank" class="border_right_link" href="http://libgen.biz">Поиск и скачивание научных статей и книг</a>
+   <a target="_blank" class="border_right_link" href="http://libgen.education/">Поиск и скачивание научных статей и книг</a>
   </div>
   <br class="break">  
   <img src="/img/dz.gif" alt="Бесплатные научные статьи" class="img_right_menu">
