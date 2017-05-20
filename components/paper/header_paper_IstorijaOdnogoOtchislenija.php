f8cecad0c97bf465ccf33cacb520a650e5fbecf8
DySprozin <kirill@anabot.ru>
2017-05-20 21:52:23 +0300

    IstorijaOdnogoOtchislenija

---
+++ components/paper/header_paper_IstorijaOdnogoOtchislenija.php
@@ -0,0 +1,15 @@
+<?php
+  $meta['title'] = ' История одного отчисления';
+  $meta['description'] = '';
+  $meta['keywords'] = '';
+  $meta['css'] = '/css/common.css';
+  $meta['head'] = 'Может, был я грешен, &nbsp;<br>
+    Может, был и свят…&nbsp;<br>
+    Мертвецам не нужно песен — &nbsp;<br>
+   Пусть спокойно спят&nbsp;
+  ';
+  $meta['menu'] = '';
+  
+  
+
+
